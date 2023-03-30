cc620d2a3d18be78b2ed90c022dac80b52059cec
Ayen <87607693+Ayen11@users.noreply.github.com>
2023-03-30 17:37:19 +0300

    added TimeAndLocation component

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function TimeAndLocation() {
   return (
-    <div>TimeAndLocation</div>
-  )
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuesday, 6 Spetember 2022 | Local time: 4:20 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Tel Aviv-Jaffa, ISR</p>
+      </div>
+    </div>
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
